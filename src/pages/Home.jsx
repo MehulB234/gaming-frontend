195bda883e88fce0bf843345a659d3aebdee94f4
Mehul Bellukutty <mehulbrainy@gmail.com>
2026-03-17 09:11:04 -0400

    github commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "../css/Home.css";
+
+const Home = () => {
+    return(
+    <main>
+        <main id = "home" className="main-content" > </main>
+        <h2> Home </h2>
+        <p> I will display on the home page</p>
+    </main>
+    );
+};
+
+export default Home;
