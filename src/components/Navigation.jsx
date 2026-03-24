58884ae99efea1057bd44971a862c397ec803d7b
Mehul Bellukutty <mehulbrainy@gmail.com>
2026-03-24 09:02:45 -0400

    Toggle Nav Changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
+import {useState} from "react";
 import "../css/Navigation.css";
 
 const Navigation = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        console.log(menuOpen);
+    };
+
     return (
         <nav id="main-nav" aria-label="Primary">
-            <ul className="main-links">
+            <a id="toggle-nav" onClick={toggleMenu} href ="#">
+                {menuOpen? (<p> &#8963; </p>) : (<p> &#8964; </p>)}
+                </a>
+            <ul className={menuOpen?"columns": "hide-small"}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/catalog">Catalog</Link></li>
                 <li><Link to="/news">News</Link></li>
